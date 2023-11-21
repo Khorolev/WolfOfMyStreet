d5b00a7b9aa7ebb5c32a83bf452941fa8acf7041
Khorolev <maltonif@gmail.com>
2023-11-21 20:38:47 +0100

    first index of to do things

---
+++ main.py
@@ -0,0 +1,26 @@
+
+# initialize TargetTokens dictionary from a list of target tokens from "ManualTokenList" in a text file
+
+
+# update TargetTokens dictionary from Moralis Money strategy webhook (verify that it exists)
+
+
+# receive webhook for buy and sell signals as JSON from Trading View
+
+# receive webhook for buy and sell signals from Moralis Money strategies
+
+# define risk management (allocation of tokens)
+
+# define logic for executing the trade
+
+# prepare trade instructions
+
+# simulate trade on Tenderly
+
+# execute trade as one-off carbon strategy
+
+# call fastlane to perform arb with best DEX
+
+# update TargetTokenTrades to log all trades
+
+# update TargetTokens with new balance
